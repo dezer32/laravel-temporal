b2ef4af51b1e60ca0353550ebd0849354bd4d34a
Dezer <i@appdevelop.ru>
2022-11-26 19:35:29 +0100

    Fix path for copy files

--- src/Providers/CoreServiceProvider.php
+++ src/Providers/CoreServiceProvider.php
@@ -21,13 +21,13 @@ class CoreServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../../config/.rr.yaml' => $this->app->basePath('.rr.yaml'),
+            __DIR__ . '/../../config/.rr.yaml' => $this->app->basePath(),
         ], 'laravel-temporal-rr.yaml');
 
         $this->publishes([
             __DIR__ . '/../../docker/php/8.1-cli/Dockerfile' => $this->app->basePath('docker/php/8.1-cli/Dockerfile'),
-            __DIR__ . '/../../docker-compose.yml' => $this->app->basePath('docker-compose.yml'),
-            __DIR__ . '/../../.env.temporal' => $this->app->basePath('.env.temporal'),
+            __DIR__ . '/../../docker-compose.yml' => $this->app->basePath(),
+            __DIR__ . '/../../.env.temporal' => $this->app->basePath(),
         ], 'laravel-temporal-docker');
     }
 
